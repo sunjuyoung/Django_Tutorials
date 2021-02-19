49bee98fbff09232c9e5f210cdffa2d732d02de3
Sun <syseoz@naver.com>
2021-02-19 22:26:47 +0900

    add FBC blog index view

--- django_demo_project/blog/views.py
+++ django_demo_project/blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def index(request):
+    # 모든 Post레코드를 가져와 저장, 데이터베이스에 쿼리를 날려 원한는 레코드를 가져올 수 있다.
+    posts = Post.objects.all().order_by('-pk');
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts':posts
+        }
+    )
